0b3046e7158b74fea744c6a2469166a2bf43da26
Braden Potter <144744976+bpotter8@users.noreply.github.com>
2024-04-11 19:33:25 -0600

    Update code.js

--- code.js
+++ code.js
@@ -16,3 +16,26 @@ function convertToAdjList(adjMatrix) {
 
     return adjList;
 }
+
+function convertToAdjMatrix(adjList) {
+    var adjMatrix = [];
+    
+    if (adjList == []){
+        return adjList;
+    }
+
+    for (var i = 0; i < adjList.length; i++) {
+        adjMatrix.push([]);
+        for (let j = 0; j < adjList.length; j++) {
+            adjMatrix[i].push(0);
+        }
+    }
+    
+    for (var i = 0; i < adjList.length; i++) {
+        for (var j = 0; j < adjList[i].length; j++) {
+            adjMatrix[i][adjList[i][j]] = 1;
+        }
+    }
+
+    return adjMatrix;
+}
